2b76be4490a063c51694dc571d0c12bbff3e9e62
TbOMu4 <artemtumoshenko@gmail.com>
2026-05-18 14:09:16 +0300

    fix image filename case-5

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import Procedure from "./pages/Procedure/Procedure";
 import Header from "./components/Header/Header";
 import MainLayout from "./components/MainLayout/MainLayout";
 import { About, Services, Contacts, PriceList, Home } from "./pages/index";
-
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/EndoRphin">
         <Header />
         <MainLayout>
           <Routes>
@@ -24,5 +23,24 @@ function App() {
     </>
   );
 }
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Header />
+//         <MainLayout>
+//           <Routes>
+//             <Route index element={<Home />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="/services" element={<Services />} />
+//             <Route path="/contacts" element={<Contacts />} />
+//             <Route path="/price-list" element={<PriceList />} />
+//             <Route path="/services/:slug" element={<Procedure />} />
+//           </Routes>
+//         </MainLayout>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
 
 export default App;
