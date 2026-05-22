8653f475a698900360e1f232136bf612dd5bd212
TbOMu4 <artemtumoshenko@gmail.com>
2026-05-22 14:56:03 +0300

    remove vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/EndoRphin/",
   plugins: [react(), tailwindcss()],
+
   css: {
     preprocessorOptions: {
       scss: {
